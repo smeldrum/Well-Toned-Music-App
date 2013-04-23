119c4b6f0802d9f034e95e2d429f71c85b335bc1
zoesobin <zoe.sobin@tufts.edu>
2013-04-23 20:06:58 -0300

    Create Note.h

---
+++ Four Part Checker/Four Part Checker/Note.h
@@ -0,0 +1,15 @@
+//
+//  Note.h
+//  Four Part Checker
+//
+//  Created by Zoe Sobin on 4/20/13.
+//  Copyright (c) 2013 Spencer Meldrum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Note: UIButton
+@property (nonatomic, assign) UIEdgeInsets touchcircle;
+@property (nonatomic, assign) NSInteger index;
+
+@end
